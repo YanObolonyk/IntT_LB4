3a21c37f30d7f8c7f3e4506955be3c0771886b4d
Warden <warden.killa@gmail.com>
2024-11-22 12:17:42 +0100

    files

--- index.php
+++ index.php
@@ -4,15 +4,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LB4</title>
+    <style>
+        div, input[type="number"], input[type="button"] {
+            display: inline-block;
+            vertical-align: middle;
+        }
+        input[type="button"] {
+            background-color: #cbc2b4;
+            padding-inline: 10px;
+        }
+    </style>
 </head>
 <body>
+Калькулятор<br><br>
+<form id="myForm" onsubmit="return validateForm(event)" method="POST">
+<input type="number" id="num1" value="0" required>
 
-    <h1>TEST</h1>
+<div>
+        <input type="radio" name="operation" value="+" required> +
+    <br>
+        <input type="radio" name="operation" value="-"> -
+    <br>
+        <input type="radio" name="operation" value="/"> /
+    <br>
+        <input type="radio" name="operation" value="*"> *
+    <br>
+
+</div>
+
+<input type="number" id="num2" value="0" required>
+
+<input type="button" value="Розрахувати" onclick="validateForm(event)">
+</form><br><br>
+
+<script>
+        function validateForm(event) {
+            event.preventDefault(); 
+            const num1 = document.getElementById('num1').value;
+            const num2 = document.getElementById('num2').value;
+            const operation = document.querySelector('input[name="operation"]:checked');
+
+            if (!num1 || !num2) {
+                alert('Заповніть числа.');
+                return;
+            }
+
+            if (!operation) {
+                alert('Оберіть операцію.');
+                return;
+            }
+
+            alert('Обчислення...');
+            document.getElementById('myForm').submit();
+                       
+        }
+</script>
+
+<h3>Результат:</h3>
+
+<h3>Історія:</h3>
 
 <?php
 
-    echo "Hello!";
-    
 ?>
 
 </body>
