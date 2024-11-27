51898d55654f3f237242e2e25acaffbc07f6c8ea
Warden <warden.killa@gmail.com>
2024-11-27 22:51:36 +0100

    files

--- index.php
+++ index.php
@@ -16,10 +16,10 @@
     </style>
 </head>
 <body>
-Калькулятор<br><br>
-<form id="myForm" onsubmit="return validateForm(event)" method="POST">
-<input type="number" id="num1" value="0" required>
+<br>Калькулятор<br><br>
 
+<form id="myForm" method="POST">
+<input type="number" id="num1" name="num1" value="0" required>
 <div>
         <input type="radio" name="operation" value="+" required> +
     <br>
@@ -29,43 +29,80 @@
     <br>
         <input type="radio" name="operation" value="*"> *
     <br>
-
 </div>
-
-<input type="number" id="num2" value="0" required>
-
-<input type="button" value="Розрахувати" onclick="validateForm(event)">
+<input type="number" id="num2" name="num2" value="0" required>
+<input type="submit" value="Розрахувати">
 </form><br><br>
 
 <script>
-        function validateForm(event) {
-            event.preventDefault(); 
-            const num1 = document.getElementById('num1').value;
-            const num2 = document.getElementById('num2').value;
-            const operation = document.querySelector('input[name="operation"]:checked');
-
-            if (!num1 || !num2) {
-                alert('Заповніть числа.');
-                return;
-            }
-
-            if (!operation) {
-                alert('Оберіть операцію.');
-                return;
-            }
-
-            alert('Обчислення...');
-            document.getElementById('myForm').submit();
-                       
-        }
+        
 </script>
 
 <h3>Результат:</h3>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the inputs from the form
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operation = $_POST['operation'];
+
+    // Initialize result variable
+    $result = "";
+
+    // Perform the calculation
+    if (is_numeric($num1) && is_numeric($num2)) {
+        switch ($operation) {
+            case "+":
+                $result = $num1 + $num2;
+                break;
+            case "-":
+                $result = $num1 - $num2;
+                break;
+            case "*":
+                $result = $num1 * $num2;
+                break;
+            case "/":
+                if ($num2 != 0) {
+                    $result = $num1 / $num2;
+                } else {
+                    $result = "Помилка: ділення на нуль!";
+                }
+                break;
+            default:
+                $result = "Невірна операція!";
+        }
+    } else {
+        $result = "Введіть дійсні числа!";
+    }
+
+    // Save the operation and result to the beginning of "history.txt"
+    $historyFile = 'history.txt';
+    $currentEntry = "$num1 $operation $num2 = $result\n";
+
+    // Prepend the new entry to the file
+    if (file_exists($historyFile)) {
+        $oldContent = file_get_contents($historyFile);
+        $newContent = $currentEntry . $oldContent;
+    } else {
+        $newContent = $currentEntry;
+    }
+    file_put_contents($historyFile, $newContent);
+
+    // Display the result
+    echo $result;
+}
+?>
+
 <h3>Історія:</h3>
 
 <?php
-
+$myfile = fopen("history.txt", "r") or die("Unable to open file!");
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
 ?>
 
 </body>
